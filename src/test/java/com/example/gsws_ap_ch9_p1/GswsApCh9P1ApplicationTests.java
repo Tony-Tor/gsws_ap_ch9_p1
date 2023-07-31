e4e6f1ae5ae918665498f3bfd33cf767bc69017e
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-31 18:00:45 +0400

    Initial commit

---
+++ src/test/java/com/example/gsws_ap_ch9_p1/GswsApCh9P1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_ch9_p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GswsApCh9P1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
